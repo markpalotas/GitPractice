44c3994e57f378e488a2d34ff6025c7f4c8f6a69
Mark Palotas <ppmark@gmail.com>
2019-01-14 14:59:53 +0100

    b5

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     }
 
-    public static void count(int age, int month) {
-        System.out.println("My age is: " + age + "month:" + month);
+    public static void count(int age, String eyeColor) {
+        System.out.println("My age is: " + age + " and I have " + eyeColor " eyes.");
     }
 }
