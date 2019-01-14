f5b7d6c82ee4ef3335bfe088d4376706d6742015
Szirmai Dóra <szirmaidora@gmail.com>
2019-01-14 14:53:30 +0100

    a3

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        count(38);
+        count(38, 2);
 
     }
 
-    public static void count(int age) {
-        System.out.println("My age is: " + age);
+    public static void count(int age, int month) {
+        System.out.println("My age is: " + age + "month:" + month);
     }
 }
